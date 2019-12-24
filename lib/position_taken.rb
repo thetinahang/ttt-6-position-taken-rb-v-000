998dc5518191045b8b481883e5f48f75f1349095
Tina Hang <mstinahang@gmail.com>
2019-12-24 20:44:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++
@@ -1,8 +0,0 @@
-# code your #position_taken? method here!
-def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    return true
-  else board[index] == " " || "" || nil
-    return false
-  end
-end
